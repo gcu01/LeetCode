dcce66e21c4b08ca65aa73495fd41e30f1edaa97
I_am_Gabriel <gabrielcampeanu@gmail.com>
2025-10-24 14:22:58 +0200

    [24] [Time Beats: 23.74%] [Memory Beats: 95.48%] - LeetPush

---
+++ single_lists/Range-Sum-Query---Immutable.py
@@ -0,0 +1,29 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        #my version
+        '''
+        sum = 0
+        for i in range(left, right+1):
+            sum += self.nums[i]        
+        return sum
+        '''
+        return sum(self.nums[left: right+1])
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
