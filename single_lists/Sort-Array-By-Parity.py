cf92612b3ee49de5d0634d0ac4c42b7672d54e71
I_am_Gabriel <gabrielcampeanu@gmail.com>
2025-11-01 18:47:32 +0100

    [905] [Time Beats: 55.95%] [Memory Beats: 26.58%] - LeetPush

--- single_lists/Sort-Array-By-Parity.py
+++ single_lists/Sort-Array-By-Parity.py
@@ -4,6 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # optimization 3 - working on the same list, rearenging elements, even at the beginning, odd at the end
+        i, j = 0, len(nums)-1
+        while i < j:
+            if nums[i]%2 != 0 and nums[j]%2==0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+            elif nums[i]%2 == 0:
+                i += 1
+            else:
+                j -=1
+        return nums
+        
+        # optimization 1 is using another list - better exec time
+        '''
+        out = []
+        for i in range(len(nums)):
+            if nums[i] == 0 or nums[i]%2 == 0:
+                out.insert(0,nums[i])
+            else:
+                out.append(nums[i])
+        return out
+        '''
+
+
+        # my version is using the same list, better memory usage
+        
+    '''
         i = len(nums)-1
         j = -1
         while i >= 0:
@@ -17,3 +45,4 @@ class Solution(object):
             nums.pop(j)
             i -= 1
         return nums
+    '''
