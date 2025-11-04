49e64f808e0ef86094602fbc9e9805925b3a7622
I_am_Gabriel <gabrielcampeanu@gmail.com>
2025-11-04 15:58:01 +0100

    [Q89] [Time Beats: 42.08%] [Memory Beats: 87.17%] - LeetPush

---
+++ single_lists/Q89.-Squares-of-a-Sorted-Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            nums[i] *= nums[i]
+        #print(nums)
+        nums.sort()
+        return nums
