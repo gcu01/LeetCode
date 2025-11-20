48b37592f8431d52de529f28d6a9b9194508dd8d
I_am_Gabriel <gabrielcampeanu@gmail.com>
2025-11-20 13:44:35 +0100

    [1672] [Time Beats: 100.00%] [Memory Beats: 18.63%] - LeetPush

---
+++ single_lists/Richest-Customer-Wealth.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        max = 0
+        for i in range(len(accounts)):
+            sum = 0
+            for j in range(len(accounts[i])):
+                sum += accounts[i][j]
+            if max < sum:
+                max = sum
+        return max
