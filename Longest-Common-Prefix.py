c34daa0f4fee5f19d206eba3343daf2407b63a78
I_am_Gabriel <gabrielcampeanu@gmail.com>
2026-06-10 18:24:09 +0100

    [14] [Time Beats: 29.59%] [Memory Beats: 96.03%] - LeetPush

--- Longest-Common-Prefix.py
+++ Longest-Common-Prefix.py
@@ -4,22 +4,58 @@
 4        :type strs: List[str]
 5        :rtype: str
 6        """
-7        out = ""
-8        
-9        if len(strs) < 1:
-10            return out
-11        elif len(strs[0])<1:
-12            return out
-13        elif len(strs) < 2:
-14            return strs[0]
-15        k = 0
-16        flag = True
-17        while flag:
-18            for i in range(1, len(strs)):                
-19                if len(strs[i]) < 1 or  k+1 > len(strs[0]) or k+1 > len(strs[i]) or strs[0][k] != strs[i][k]:
-20                    flag = False
-21            if flag == True:                
-22                out += strs[0][k]
-23                k += 1
-24            
-25        return out
+7        res = strs[0]
+8        for i in range(1, len(strs)):
+9            if res == "":
+10                return res
+11            length = min( len(res), len(strs[i]) )
+12            res = res[:length]
+13            #print("res = ", res)
+14            #print("length = ", length)
+15            for idx in range(length):
+16                #print("idx = ", idx)
+17                if res[idx] != strs[i][idx]:
+18                    res = res[:idx]
+19                    #print("new res=", res)
+20                    break
+21        return res
+22
+23        
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43        out = ""
+44        
+45        if len(strs) < 1:
+46            return out
+47        elif len(strs[0])<1:
+48            return out
+49        elif len(strs) < 2:
+50            return strs[0]
+51        k = 0
+52        flag = True
+53        while flag:
+54            for i in range(1, len(strs)):                
+55                if len(strs[i]) < 1 or  k+1 > len(strs[0]) or k+1 > len(strs[i]) or strs[0][k] != strs[i][k]:
+56                    flag = False
+57            if flag == True:                
+58                out += strs[0][k]
+59                k += 1
+60            
+61        return out
