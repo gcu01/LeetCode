7e054a91b0e0975fdd46c6ca6a8b1a2517cd91c3
I_am_Gabriel <gabrielcampeanu@gmail.com>
2026-06-10 17:14:06 +0100

    [1] [Time Beats: 42.22%] [Memory Beats: 16.41%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -5,8 +5,15 @@
 5        :type target: int
 6        :rtype: List[int]
 7        """
-8        for i, val in enumerate(nums):
-9            if target-val in nums[i+1:]:
-10                j = i+1 + nums[i+1:].index(target-val)
-11                return[i, j]
-12        
+8        seen = dict()
+9        for i, val in enumerate(nums):
+10            #print("target-val=", target-val)
+11            if target - val in seen.values():
+12                for k, v in seen.items():
+13                    if v == target-val:
+14                        return [k, i]
+15            else:
+16                seen[i] = val
+17                #print("seen=", seen.items())
+18            
+19        
